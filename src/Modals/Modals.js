bde6e9bacc021ab4b2264817f4d8ab5bbfaf5c48
evanhurd <evan.hurd@renweb.com>
2015-09-14 17:33:02 -0500

    Update Modals.js

--- src/Modals/Modals.js
+++ src/Modals/Modals.js
@@ -1,1 +1,67 @@
-https://github.com/evanhurd/Bookdex.git
+var System =  new QuickStep.Model('System', [
+	  'User'
+	, 'Login'
+	, 'plugins'
+]);
+
+var User = new QuickStep.Model('Book', [
+	  'id'
+	, 'username'
+	, 'password'
+	, 'firstName'
+	, 'lastName'
+	, 'email'
+
+	, 'Books'
+	, 'Entries'
+	, 'Quotes'
+	, 'Session'
+]);
+
+User.prototype.init = function(){
+	this.Books = new QuickStep.Collection(Books);
+	this.Entries = new QuickStep.Collection(Entries);
+	this.Quotes = new QuickStep.Collection(Quotes);
+};
+
+var Session = new QuickStep.Model('Session', [
+	  'token'
+	, 'loginTime'
+	, 'userId'
+	, 'state'
+]);
+
+var Book = new QuickStep.Model('Book', [
+	  'id'
+	, 'title'
+	, 'author'
+
+	, 'Entires'
+	, 'Quotes'
+]);
+
+var Entry = new QuickStep.Model('Entry', [
+	  'id'
+	, 'name'
+
+	, 'Locations'
+]);
+
+var Location = new QuickStep.Model('Location', [
+	  'id'
+	, 'entryId'
+	, 'bookId'
+	, 'from'
+	, 'to'
+	, 'noteNumber'
+
+	, 'Book'
+]);
+
+var Quote = new QuickStep.Model('Comment', [
+	  'id'
+	, 'locationId'
+	, 'text'
+
+	, 'Locations'	
+]);
